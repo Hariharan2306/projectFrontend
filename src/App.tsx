b7c02c535763e4d9fa99195228deff980f9c2982
Hariharan <hariharan.j@basiscloudsolutions.com>
2024-12-29 17:15:11 +0530

    added icons and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { lazy, Suspense } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+const Login = lazy(() => import("./components/Login"));
+const Home = lazy(() => import("./components/Home"));
 
 function App() {
-  return <></>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </Suspense>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { StrictMode } from "react";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./apis/store";
-import React from "react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
