c76723da0169d1ff65aeaac19fa663c64da2ef33
Hariharan <hariharan.j@basiscloudsolutions.com>
2025-01-17 22:01:41 +0530

    fixed types

--- src/reducers/userReducer.ts
+++ src/reducers/userReducer.ts
@@ -3,11 +3,15 @@ import { UserTypes } from "../actions/actionTypes";
 type userReducerTypes = {
   successMessage: string;
   error: string;
+  type?: string;
 };
 
 const initialReducer: userReducerTypes = { successMessage: "", error: "" };
 
-export const userReducer = (state = initialReducer, action: any) => {
+export const userReducer = (
+  state = initialReducer,
+  action: userReducerTypes
+) => {
   switch (action.type) {
     case UserTypes.SUCCESS_CREATE_USER_DETAILS:
     case UserTypes.SUCCESS_LOGIN_USER:
